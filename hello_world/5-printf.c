5aeed42130964e88ea73aacb510a5ac43a222ca3
lucas <zaleslucas3@gmail.com>
2026-02-09 09:29:01 +0000

    task 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a message with proper grammar, followed by a new line
+ *
+ * Return: 0 (success)
+ */
+	int main(void)
+		{
+			printf("with proper grammar, but the outcome is a piece of art,")
+				return (0);
+		}
