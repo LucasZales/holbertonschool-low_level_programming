b9ca2e45f6736e7260ac55d926dfffda17dc4e32
lucas <zaleslucas3@gmail.com>
2026-02-09 23:19:01 +0000

    task 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints a message with proper grammar, followed by a new line
- *
- * name - lucas 
- *
- * Return: 0 (success)
- */
+* main: starting point of the programm
+*
+* name:lucas
+*
+* Return: 0 (success)
+*/
 	int main(void)
 		{
 			printf("with proper grammar, but the outcome is a piece of art,");
