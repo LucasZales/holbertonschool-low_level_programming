2027a0f0b173d9a401d3a8ac5e98865a4c9a0033
lucas <zaleslucas3@gmail.com>
2026-02-09 06:58:07 +0000

    task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,11 +2,13 @@
 
 /**
  * main - Entry point
+ *
  * Description: Prints a message using puts
- * Return: Always 0 (success)
+ *
+ * Return: 0 (success)
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
