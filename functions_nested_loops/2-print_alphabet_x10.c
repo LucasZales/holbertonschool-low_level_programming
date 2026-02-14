7a81ce3ee1310f83d70120063181dd5fd3462543
lucas <zaleslucas3@gmail.com>
2026-02-14 08:50:33 +0000

    task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10-Description.
+ *
+ *Return:0
+ */
+
+void print_alphabet_x10(void)
+{
+int a = '0';
+while (a < 10)
+{
+char b = 'a';
+while ( b <= 'z')
+{
+_putchar(b);
+b++;
+}
+_putchar('\n');
+a++;
+}
+}
