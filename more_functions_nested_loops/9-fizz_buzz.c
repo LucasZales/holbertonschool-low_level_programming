965ad7e444741bbfb1852169ab192fd71b413f28
lucas <zaleslucas3@gmail.com>
2026-02-17 01:48:07 +0000

    task 9 goes first

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *main-description
+ *Return:0 when complete.
+ */
+
+int main(void)
+	{
+	int Num = 1;
+
+	while (Num <= 100)
+	{
+	if (Num % 3 == 0 && Num % 5 == 0)
+	{
+	printf("FizzBuzz ");
+	}
+	else if (Num % 3 == 0)
+	{
+	printf("Fizz ");
+	}
+	else if (Num % 5 == 0)
+	{
+	printf("Buzz ");
+	}
+	else
+	{
+	printf("%d ", Num);
+	}
+	Num++;
+	}
+	return (0);
+	}
