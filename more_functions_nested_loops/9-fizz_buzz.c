aaa51a8a5b15748a3a0042a0672fbda943a5c10c
lucas <zaleslucas3@gmail.com>
2026-02-17 02:03:32 +0000

    task 9 first

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,21 +13,24 @@ int main(void)
 	{
 	if (Num % 3 == 0 && Num % 5 == 0)
 	{
-	printf("FizzBuzz ");
+	printf("FizzBuzz");
 	}
 	else if (Num % 3 == 0)
 	{
-	printf("Fizz ");
+	printf("Fizz");
 	}
 	else if (Num % 5 == 0)
 	{
-	printf("Buzz ");
+	printf("Buzz");
 	}
 	else
 	{
-	printf("%d ", Num);
+	printf("%d", Num);
 	}
+	if(Num != 100)
+	printf(" ");
 	Num++;
 	}
+	printf("\n");
 	return (0);
-	}
+	}	
