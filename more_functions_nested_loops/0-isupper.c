1bfeb7a90b9c7e6ac54b6f0aed36ce60c6bb566f
lucas <zaleslucas3@gmail.com>
2026-02-17 04:42:50 +0000

    task 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_isupper - description
+ *@c: integer
+ *Return:0 when complete.
+ */
+
+int _isupper(int c)
+{
+if (c <= 90 && c >= 65)
+{
+return (1);
+}
+return (0);
+}
