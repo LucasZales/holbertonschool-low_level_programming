7a61f2f047cf7632468b483854020c5d22fefc71
lucas <zaleslucas3@gmail.com>
2026-02-21 10:06:18 +0000

    task 4

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: pointer to the string to print
+ *
+ * Description: prints each character of the string
+ * pointed to by str using _putchar, then prints '\n'.
+ */
+void _puts(char *str)
+{
+int i = 0;
+while (str[i] != '0')
+{
+_putchar(str[i]);
+i++;
+}
+}
