1a7766c6c64e7affc8dcd3232e460a59baebf8da
lucas <zaleslucas3@gmail.com>
2026-03-08 06:49:36 +0000

    task 1 betty again

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - represents a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+
 #include <stddef.h>
 
 struct dog
