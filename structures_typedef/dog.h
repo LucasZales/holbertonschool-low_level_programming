4192747ef627dfffc4962cbc2ba61de01ac9ec69
lucas <zaleslucas3@gmail.com>
2026-03-08 06:45:34 +0000

    task 1

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,19 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * struct dog - defines a dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- *
- * Description: structure about a poppyyy
- */
+#include <stddef.h>
+
 struct dog
 {
-char *name;
-float age;
-char *owner;
+    char *name;
+    float age;
+    char *owner;
 };
 
-#endif /* DOG_H */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
