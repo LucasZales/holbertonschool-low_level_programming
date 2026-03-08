375cb8b5200cef9a2c0bce1dfece387ed65613cd
lucas <zaleslucas3@gmail.com>
2026-03-08 06:54:48 +0000

    task 2

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,16 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stddef.h>
+
 /**
  * struct dog - represents a dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  */
-
-
-#include <stddef.h>
-
 struct dog
 {
 char *name;
@@ -19,5 +17,6 @@ char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
