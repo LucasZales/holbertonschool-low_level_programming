f1ddd281eb6ae9c053c27e7ea801a016172c93b0
lucas <zaleslucas3@gmail.com>
2026-03-28 23:30:51 +0000

    old task pointers

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,47 +1,37 @@
+#include <stdio.h>
 #include "main.h"
+#include <limits.h>
 
 /**
- * _atoi - converts a string to an integer
- * @s: the string to convert
- *
- * Return: the integer value represented by the string
- */
-#include "main.h"
+*_atoi-function returns the first integer in a string as type int
+*@s:input string
+*Return:returns the first number as type int
+*/
 
-/**
- * _atoi - converts a string to an integer
- * @s: string to convert
- *
- * Return: integer value represented by the string
- */
-#include "main.h"
-
-/**
- * _atoi - converts a string to an integer
- * @s: string to convert
- *
- * Return: integer value represented by the string
- */
 int _atoi(char *s)
 {
 int i = 0;
-int sign = 1;
-int num = 0;
+int digits = 0;
+int sign = -1;
+int first_digit = 0;
+
 while (s[i] != '\0')
 {
-if (s[i] == '-')
-sign *= -1;
-else if (s[i] == '+')
-i++;
-else if (s[i] >= '0' && s[i] <= '9')
-break;
-else
-i++;
-}
-while (s[i] >= '0' && s[i] <= '9')
+if (s[i] == '-' && !first_digit)
 {
-num = num * 10 + (s[i] - '0');
+sign *= -1;
+}
+else if (s[i] >= '0' && s[i] <= '9')
+{
+first_digit = 1;
+digits = digits * 10 - (s[i] - '0');
+}
+else if (first_digit)
+{
+break;
+}
 i++;
 }
-return (sign * num);
+digits *= sign;
+return (digits);
 }
